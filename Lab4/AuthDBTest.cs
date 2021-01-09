a43a5563bd54fd2b1d738f5d2a1b714b13b459e6
otschebutsch <55258053+otschebutsch@users.noreply.github.com>
2021-01-09 11:13:49 +0200

    let's go

---
+++ Lab4/AuthDBTest.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using Xunit;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.PasswordHashingUtils;
+
+namespace Lab4
+{
+    public class AuthDBTest
+    {
+        private const string Server = @"DESKTOP-6V7SVAP\OTCHEBUCH";
+        private const string Database = @"IIG.CoSWE.AuthDB";
+        private const bool IsTrusted = false;
+        private const string Login = @"sa";
+        private const string Password = @"gabartem";
+        private const int ConnectionTime = 75;
+        private AuthDatabaseUtils authDatabaseUtils = new AuthDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTime);
+
+        [Fact]
+        public void AddNormalCreds()
+        {
+            string login = "user";
+            string password = PasswordHasher.GetHash("password");
+            Assert.True(authDatabaseUtils.AddCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void AddVariousCreds()
+        {
+            Assert.True(authDatabaseUtils.AddCredentials("ґєїёэ", PasswordHasher.GetHash("ґєїёэ")));
+            Assert.True(authDatabaseUtils.AddCredentials("123", PasswordHasher.GetHash("123")));
+            Assert.True(authDatabaseUtils.AddCredentials("畝俱樂部迷", PasswordHasher.GetHash("畝俱樂部迷")));
+            Assert.True(authDatabaseUtils.AddCredentials(".,<>?#~![]{}", PasswordHasher.GetHash(".,<>?#~![]{}")));
+            Assert.True(authDatabaseUtils.AddCredentials("✔️✔️✔️", PasswordHasher.GetHash("✔️✔️✔️")));
+            Assert.True(authDatabaseUtils.AddCredentials("usermeme", PasswordHasher.GetHash("P A S S W O R D")));
+        }
+
+
+        [Fact]
+        public void AddSamePswdCreds()
+        {
+            string login = "user123";
+            string password = PasswordHasher.GetHash("password");
+            Assert.True(authDatabaseUtils.AddCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void AddEmptyLoginCreds()
+        {
+            string login = "";
+            string password = PasswordHasher.GetHash("password");
+            Assert.False(authDatabaseUtils.AddCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void AddNullLoginCreds()
+        {
+            string login = null;
+            string password = PasswordHasher.GetHash("password");
+            Assert.False(authDatabaseUtils.AddCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void AddEmptyPswdCreds()
+        {
+            string login = "user2";
+            string password = PasswordHasher.GetHash("");
+            Assert.True(authDatabaseUtils.AddCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void AddNullPswdCreds()
+        {
+            string login = "user3";
+            Assert.Throws<ArgumentNullException>(() => authDatabaseUtils.AddCredentials(login, PasswordHasher.GetHash(null)));
+        }
+
+        
+        [Fact]
+        public void UpdatePswdCreds()
+        {
+            authDatabaseUtils.AddCredentials("userup", PasswordHasher.GetHash("password"));
+            Assert.True(authDatabaseUtils.UpdateCredentials("userup", PasswordHasher.GetHash("password"), "userup", PasswordHasher.GetHash("newpassword")));
+        }
+
+
+        [Fact]
+        public void UpdateSameCreds()
+        {
+            authDatabaseUtils.AddCredentials("userzhb", PasswordHasher.GetHash("passworddd"));
+            Assert.True(authDatabaseUtils.UpdateCredentials("userzhb", PasswordHasher.GetHash("passworddd"), "userzhb", PasswordHasher.GetHash("passworddd")));
+        }
+
+
+        [Fact]
+        public void UpdateWrongCreds()
+        {
+            Assert.False(authDatabaseUtils.UpdateCredentials("notuser", PasswordHasher.GetHash("notpassword"), "user5", PasswordHasher.GetHash("password")));
+        }
+
+
+        [Fact]
+        public void UpdateNotHashedPswdCreds()
+        {
+            authDatabaseUtils.AddCredentials("userhash", PasswordHasher.GetHash("password"));
+            Assert.False(authDatabaseUtils.UpdateCredentials("userhash", PasswordHasher.GetHash("password"), "userhash", "password"));
+        }
+        
+        
+        [Fact]
+        public void CheckCredsTest()
+        {
+            string login = "check";
+            string password = PasswordHasher.GetHash("checkpswd");
+            authDatabaseUtils.AddCredentials(login, password);
+            Assert.True(authDatabaseUtils.CheckCredentials(login, password));
+        }
+
+
+        [Fact]
+        public void CheckWrongCreds()
+        {
+            Assert.False(authDatabaseUtils.CheckCredentials("notuser", PasswordHasher.GetHash("notpassword")));
+        }
+
+
+        [Fact]
+        public void CheckWrongPswdCreds()
+        {
+            Assert.False(authDatabaseUtils.CheckCredentials("user", PasswordHasher.GetHash("notpassword")));
+        }
+
+        [Fact]
+        public void CheckWrongLoginCreds()
+        {
+            Assert.False(authDatabaseUtils.CheckCredentials("notuser", PasswordHasher.GetHash("password")));
+        }
+
+
+        [Fact]
+        public void DeleteWrongPswdCreds()
+        {
+            Assert.False(authDatabaseUtils.DeleteCredentials("user", PasswordHasher.GetHash("notpassword")));
+        }
+
+
+        [Fact]
+        public void DeleteCredsTest()
+        {
+            Assert.True(authDatabaseUtils.DeleteCredentials("user123", PasswordHasher.GetHash("password")));
+            Assert.True(authDatabaseUtils.DeleteCredentials("user2", PasswordHasher.GetHash("")));
+        }
+        
+
+        [Fact]
+        public void DeleteWrongCreds()
+        {
+            Assert.False(authDatabaseUtils.DeleteCredentials("notuser", PasswordHasher.GetHash("notpassword")));
+        }
+    }
+}
---
+++ Lab4/StorageDBTest.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Xunit;
+using IIG.CoSFE.DatabaseUtils;
+using IIG.Core.FileWorkingUtils;
+using System.Text;
+
+namespace Lab4
+{
+    public class StorageBDTest
+    {
+        private const string Server = @"DESKTOP-6V7SVAP\OTCHEBUCH";
+        private const string Database = @"IIG.CoSWE.StorageDB";
+        private const bool IsTrusted = false;
+        private const string Login = @"sa";
+        private const string Password = @"gabartem";
+        private const int ConnectionTime = 75;
+        private StorageDatabaseUtils storageDatabaseUtils = new StorageDatabaseUtils(Server, Database, IsTrusted, Login, Password, ConnectionTime);
+
+        [Fact]
+        public void AddFileTest()
+        {
+            Assert.True(storageDatabaseUtils.AddFile("testfile.txt", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testfile.txt"))));
+            Assert.True(storageDatabaseUtils.AddFile("db.accdb", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\db.accdb"))));
+            Assert.True(storageDatabaseUtils.AddFile("docfile.docx", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\docfile.docx"))));
+            Assert.True(storageDatabaseUtils.AddFile("pdftest.pdf", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\pdftest.pdf"))));
+            Assert.True(storageDatabaseUtils.AddFile("archive.zip", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\archive.zip"))));
+            Assert.True(storageDatabaseUtils.AddFile("picture.jpg", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\picture.jpg"))));
+            Assert.True(storageDatabaseUtils.AddFile("present.pptx", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\present.pptx"))));
+            Assert.True(storageDatabaseUtils.AddFile("✔️✔️✔️.txt", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\✔️✔️✔️.txt"))));
+            Assert.True(storageDatabaseUtils.AddFile("ґєїъэё.txt", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\ґєїъэё.txt"))));
+            Assert.True(storageDatabaseUtils.AddFile("mathcad.xmcd", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\mathcad.xmcd"))));
+            Assert.True(storageDatabaseUtils.AddFile("somepng.png", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\somepng.png"))));
+            Assert.True(storageDatabaseUtils.AddFile("Відео-файл.mp4", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\Відео-файл.mp4"))));
+            Assert.True(storageDatabaseUtils.AddFile("music.mp3", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\music.mp3"))));
+            Assert.True(storageDatabaseUtils.AddFile("apktest.apk", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\apktest.apk"))));
+            Assert.True(storageDatabaseUtils.AddFile("someexe.exe", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\someexe.exe"))));
+            Assert.True(storageDatabaseUtils.AddFile("HUH", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\HUH"))));
+            Assert.True(storageDatabaseUtils.AddFile("oops.torrent", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\oops.torrent"))));
+            Assert.True(storageDatabaseUtils.AddFile(".txt", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\.txt"))));
+        }
+
+
+        [Fact]
+        public void AddFileWithEMptyName()
+        {
+            Assert.False(storageDatabaseUtils.AddFile("", Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testfile.txt"))));
+        }
+
+
+        [Fact]
+        public void AddFileWithNullContent()
+        {
+            Assert.False(storageDatabaseUtils.AddFile("null.txt", null));
+        }
+
+
+        [Fact]
+        public void AddFileWithEmptyContent()
+        {
+            Assert.True(storageDatabaseUtils.AddFile("empty.txt", new byte[0]));
+        }
+
+
+        [Fact]
+        public void GetFileTrue()
+        {
+            string name;
+            byte[] content;
+            Assert.True(storageDatabaseUtils.GetFile(670, out name, out content));
+        }
+
+
+        [Fact]
+        public void GetFileTest()
+        {
+            string name;
+            byte[] content;
+            Assert.True(storageDatabaseUtils.GetFile(627, out name, out content));
+            Assert.Equal("testfile.txt", name);
+            Assert.Equal(Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\testfile.txt")), content);
+            storageDatabaseUtils.GetFile(628, out name, out content);
+            Assert.Equal("db.accdb", name);
+            Assert.Equal(Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\db.accdb")), content);
+            storageDatabaseUtils.GetFile(634, out name, out content);
+            Assert.Equal("✔️✔️✔️.txt", name);
+            Assert.Equal(Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\✔️✔️✔️.txt")), content);
+            storageDatabaseUtils.GetFile(635, out name, out content);
+            Assert.Equal("ґєїъэё.txt", name);
+            Assert.Equal(Encoding.ASCII.GetBytes(FileWorker.GetFileName(@"C:\testing\ґєїъэё.txt")), content);
+        }
+
+
+        [Fact]
+        public void GetWrongFile()
+        {
+            Assert.False(storageDatabaseUtils.GetFile(0, out _, out _));
+            Assert.False(storageDatabaseUtils.GetFile(25, out _, out _));
+            Assert.False(storageDatabaseUtils.GetFile(-1, out _, out _));
+        }
+
+
+        [Fact]
+        public void GetFilesTest()
+        {
+            Assert.NotNull(storageDatabaseUtils.GetFiles("testfile.txt"));
+        }
+
+
+        [Fact]
+        public void DeleteFileTest()
+        {
+            Assert.True(storageDatabaseUtils.DeleteFile(0));
+            Assert.True(storageDatabaseUtils.DeleteFile(-1));
+            Assert.True(storageDatabaseUtils.DeleteFile(266));
+            Assert.True(storageDatabaseUtils.DeleteFile(267));
+            Assert.True(storageDatabaseUtils.DeleteFile(268));
+            Assert.True(storageDatabaseUtils.DeleteFile(269));
+        }
+    }
+}
